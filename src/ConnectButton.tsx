7d66302edf942d13e200fb0f7976230402f076c7
Alaa Saleh <alaaisale7@gmail.com>
2023-08-21 18:49:14 -0300

    connect button

--- src/ConnectButton.tsx
+++ src/ConnectButton.tsx
@@ -5,11 +5,26 @@ import { ConnectButton as RKConnectButton } from '@rainbow-me/rainbowkit'
 export const ConnectButton = () => {
   return (
     <RKConnectButton.Custom>
-      {() => (
-        <div>
-          <Button variant="default">CONNECT WALLET</Button>
-        </div>
-      )}
+      {({ openAccountModal, openConnectModal, mounted, account, chain }) => {
+        const connected = mounted && account && chain
+
+        if (connected)
+          return (
+            <div>
+              <Button variant="default" onClick={openAccountModal}>
+                {account.displayName}
+              </Button>
+            </div>
+          )
+
+        return (
+          <div>
+            <Button variant="default" onClick={openConnectModal}>
+              CONNECT WALLET
+            </Button>
+          </div>
+        )
+      }}
     </RKConnectButton.Custom>
   )
 }
